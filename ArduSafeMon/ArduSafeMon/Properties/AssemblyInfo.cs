90b38169b18febf3e2fcc5d2a01b8a913f46e096
dalex <dalex@local>
2026-04-13 20:20:34 +0200

    fix: add InternalsVisibleTo for test project access to internal members

--- ArduSafeMon/ArduSafeMon/Properties/AssemblyInfo.cs
+++ ArduSafeMon/ArduSafeMon/Properties/AssemblyInfo.cs
@@ -11,3 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("0EA229E6-6F0C-4B9D-BAAC-A4447204D708")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ArduSafeMon.Tests")]
