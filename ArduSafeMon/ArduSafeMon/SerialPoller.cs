be7f2763ba6ef29eef4a7bb1119daf98e3762147
dalex <dalex@local>
2026-04-13 20:15:37 +0200

    fix: add NewLine delimiter to SerialPort in Start and TryReconnect

--- ArduSafeMon/ArduSafeMon/SerialPoller.cs
+++ ArduSafeMon/ArduSafeMon/SerialPoller.cs
@@ -41,8 +41,9 @@ namespace ASCOM.ArduSafeMon
         {
             _serialPort = new SerialPort(_portName, 9600)
             {
-                ReadTimeout = 1000,
-                WriteTimeout = 1000
+                ReadTimeout  = 1000,
+                WriteTimeout = 1000,
+                NewLine      = "#"   // protocole Arduino : délimiteur '#'
             };
             _serialPort.Open();
 
@@ -121,8 +122,9 @@ namespace ASCOM.ArduSafeMon
                     _serialPort?.Dispose();
                     _serialPort = new SerialPort(_portName, 9600)
                     {
-                        ReadTimeout = 1000,
-                        WriteTimeout = 1000
+                        ReadTimeout  = 1000,
+                        WriteTimeout = 1000,
+                        NewLine      = "#"   // protocole Arduino : délimiteur '#'
                     };
                     _serialPort.Open();
                     Thread.Sleep(500);
